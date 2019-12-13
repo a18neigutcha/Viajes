c285b1b0c786f2fed2013a1ade666e36cd1fd724
unknown <a17adrposmon@inspedralbes.cat>
2019-12-13 13:06:03 +0100

    creacion formulario login parte front

--- js/script.js
+++ js/script.js
@@ -1,32 +1,87 @@
+/*function muestraExperiencias(datos){
+    var contExp=document.getElementById("contExp");
+    console.log(contExp);
+    console.log(datos);
+    for(let i=0;i<datos.length;i++){
+        let divExp=document.createElement("div");
+        divExp.setAttribute("id",i+"-exp");
+        divExp.innerHTML=datos[i].titol;
+        contExp.appendChild(divExp);
+        console.log(contExp);
+    }
+
+}*/
+
+var datosInicio;
+
 window.onload = function() {
 
-    axios.get('api.php', {
+    /*axios.get('api.php', {
         params: {
         }
       })
       .then(function (response) {
         console.log(response);
+        muestraExperiencias(response.data);
       })
       .catch(function (error) {
         console.log(error);
       })
       .finally(function () {
-        // always executed
-      }); 
-      /* 
-    $.ajax({
-        url: 'https://randomuser.me/api/',
-        dataType: 'json',
-        success: function(data) {
-    
-            generaUsuario(data);
-          
-    
-    
+        
+      }); */
+
+      var modelo={
+        init:function(){
+
+            axios.get('api.php', {
+                params: {
+                }
+            })
+            .then(function (response) {
+                datosInicio=response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            .finally(function () {
+                
+            }); 
+        },
+        dameDatosIniciales:function(){
+            return datosInicio;
         }
-      });
-      */
+
+      }
+
+      var controlador={
+          init:function(){
+            modelo.init();
+            let datos=modelo.dameDatosIniciales();
+            view.mostrarExperiencias(datos);
+          }
+          
+
+      }
+
+      var view={
+          init:function(){
+              
+
+          },
+          mostrarExperiencias:function(datos){
+            var contExp=document.getElementById("contExp");
+            for(let i=0;i<datos.length;i++){
+                let divExp=document.createElement("div");
+                divExp.setAttribute("id",i+"-exp");
+                divExp.innerHTML=datos[i].titol;
+                contExp.appendChild(divExp);
+            }
+          }
 
 
+      }
+
+      controlador.init();
 
 };
