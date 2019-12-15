36a02f1cadca0ad9e612653944fa8bc575d355cb
Viaje <a18neigutcha@inspedralbes.cat>
2019-12-15 11:47:01 +0100

    Modificasion para el despliege de la pagina web en la cuenta de Neil

--- .gitignore/login.php
+++ .gitignore/login.php
@@ -1,8 +1,8 @@
 <?php
-
-    $db_name = "viajes";
+    //Son las credenciales para acceder a la base de datos de Neil
+    $db_name = "a18NeiGutCha_viajes";
     $db_host = "localhost";
-    $db_user = "root";
+    $db_user = "a18NeiGutCha_V";
     $db_pass = "password";
 
 ?>
