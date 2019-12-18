d540767961b363e7aa1f742076dbc5889fff898c
Viaje <a18neigutcha@inspedralbes.cat>
2019-12-18 16:17:37 +0100

    Trabajando en la parte modelo,vista,controlador del admin para generar la pagina inicial

--- bd/controller.php
+++ bd/controller.php
@@ -5,14 +5,53 @@ require_once 'view.php';
 
 class controller {
   
-  public function cargaDatosInicio () {     
-
-
-    $exp = new experiencia();
+  //rutes o esdeveniments possibles
+  //view1: nom i edat
+  //view2: nom i alçada
+  private $peticions = array('view1', 'view2');
+  
+  public function handler () {
+    
+    // Què em demanen?
+    $event = 'inici';
+    
+    $uri = $_SERVER['REQUEST_URI'];
+    echo $uri;
+ 
+    foreach ($this->peticions as $peticio)
+      if (strpos($uri,$peticio) == true)
+        $event = $peticio;
+          
+    $cat = new categories();
+    
     $view = new view();
+    
+    switch ($event) {
+        
+        
+        case 'view1':
+          $dades = $cat->selectAll(array("nom","edat"));
+          $view->retornar_vista($event, $dades);
+          break;
+        
+        case 'view2':
+          $dades = $cat->selectAll(array("nom","alcada"));
+          $view->retornar_vista($event, $dades);
+          break;
+        
+        case 'inici':
+            $view->retornar_vista($event, array());
+        
+              
+        }
+      
+  }
 
-    $dades = $exp->selectAll(array("*"));
-    $view->retornarVista($dades);     
-  } 
+  
 }
+
+
+
+
+
 ?>
--- bd/view.php
+++ bd/view.php
@@ -1,28 +1,85 @@
 <?php
 
-	class view {
+class view {
+  
+  private $diccionari = array (
+    'subtitle' => array ('inici' => 'Categorias',
+                         'view1' => 'Mostrar dades nom i edat',
+                         'view2' => 'Mostrar dades nom i alcada'),
+    'capçalera' => array ('view1' => array('nom','edat'),
+                         'view2' => array('nom','alcada')));
+  
+
+public function retornar_vista ($vista, $dades=array(), $message="Dades de l'usuari") {
 	
-		//De momento no es necesario trabajar con diccionarios
-		private $diccionari = array ('experiencias');
+	// the main template is read (menu, message and the main body (a form or select result)
+        // read entire file into a string
+	$html = file_get_contents("../admin/site_media/html/persones_template.html");
+        //print_r ($html);
+	
+	// subtitle of the page is writen 
+	$html = str_replace('{subtitle}', $this->diccionari['subtitle'][$vista], $html);
+        //print_r($html);
+	
+	// message passed by controller is writen 
+	$html = str_replace('{message}', $message, $html);
+        //print_r($html);
+	
+	// the HTML table with the select result is built 
+	if ($vista=='view1' || $vista=='view2' ) {
 		
+		// the view template is read and its contents is included in the main template
+		$view = file_get_contents('../admin/site_media/html/view_template.html');
+		$html = str_replace ('{main}', $view, $html);
+		
+		// the table header is built and writen on the template
+		$capçalera = $this->buildHeader ($vista);
+		$html = str_replace('{capçalera}', $capçalera,$html);
+		
+		// the table contents is built and writen on the template
+		$contingut = $this->buildContents ($dades);
+		$html = str_replace('{contingut}', $contingut, $html);
+	}
+	
+	 
+	if ($vista=='inici' || ($vista=='view_select' && count($dades)==0) || $vista=='view_insert')
+		$vista='form_select';
 
-		public function retornarVista ($dades) {
-			
-			$html = file_get_contents('Modelo(index).html');
-
-			$html = str_replace('{experiencias}', $this->buildExp($dades), $html);
-
-			print $html;
-		} 
 
 
-		private function buildExp($dades) {
-			$str = "";
-			foreach($dades as $row){
-				$str.="<p>".$row['titol']."<p>";
-			}
-			return $str;
-		}
-	} 
+	print $html;
+} 
 
+
+private function buildHeader ($vista) {
+	$str = "";
+	foreach ($this->diccionari['capçalera'][$vista] as $value) 
+		$str .= "<th>" . $value . "</th>";
+	return $str;
+} 
+
+
+private function buildContents ($dades) {
+	$str = "";
+	for ($i=0; $i<count($dades); $i++) {
+		$str .= "<tr>";
+		foreach ($dades[$i] as $value) 
+			$str .= "<td>$value</td>";
+		$str .= "</tr>";
+	}
+	return $str;
+}
+
+
+private function buildForm ($vista) {
+	$str = "";
+	foreach ($this->diccionari['form'][$vista] as $value) {
+	
+		$str .= "<div> $value </div>";
+		$str .= "<div><input type='text' name='$value' id='$value'></div>";	
+	}	
+	return $str;
+	}
+
+}
 ?>
